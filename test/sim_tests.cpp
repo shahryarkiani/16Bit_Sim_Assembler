fd16b0f9ebe4a17502bf1f48d1950222b93c6171
Shahryar K <shahryarkiani04@gmail.com>
2023-05-18 19:44:11 -0400

    Fix Simulator load upper immediate test

--- test/sim_tests.cpp
+++ test/sim_tests.cpp
@@ -2,7 +2,7 @@
 #include "../src/Simulator.h"
 
 TEST_CASE("TEST LOAD UPPER IMMEDIATE") {
-    auto sim = Simulator({0x6555});//An instruction to load 0x155 into r1
+    auto sim = Simulator({0x6555});//An instruction to load 0x155 into upper 10 bits r1
     sim.step();
-    assert(sim.getRegisters()[1] == 0x155);
+    REQUIRE(sim.getRegisters()[1] == (0x155 << 6));
 }
