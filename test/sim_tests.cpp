4d3ab9e2c45b7f884ac5678ae246a3fac82d2233
Shahryar K <shahryarkiani04@gmail.com>
2023-05-19 18:36:56 -0400

    Write additional test cases, including load word and branch

--- test/sim_tests.cpp
+++ test/sim_tests.cpp
@@ -36,3 +36,31 @@ TEST_CASE("TEST STORE WORD OPCODE") {
     sim.step();
     REQUIRE(sim.getMemory()[32 + 63] == 55);
 }
+
+TEST_CASE("TEST LOAD WORD OPCODE") {
+    //Store 2 in r2, then load M[r2 + imm] into r1, where imm = 3, and 0xABCD is stored at that location
+    auto sim = Simulator({0x2902, 0xA503, 0, 0, 0, 0xABCD});
+    sim.step();
+    sim.step();
+    REQUIRE(sim.getRegisters()[1] == 0xABCD);
+}
+
+TEST_CASE("TEST ZERO OUT REGISTER WITH LUI AFTER ADD-I") {
+    //Stores 32 in r1, then loads upper immediate of 0, which should zero out r1
+    auto sim = Simulator({0x24A0, 0x6400});
+    sim.step();
+    REQUIRE(sim.getRegisters()[1] == 32);//Check that it was loaded
+    sim.step();
+    REQUIRE(sim.getRegisters()[1] == 0);
+}
+
+TEST_CASE("TEST BRANCH WITH EQUAL REGISTERS") {
+    //Loads 5 into r1 and r2, then compares and jumps to pc + 1 + 3 instructions ahead if r1 == r2
+    //Once there, it adds 53 to r3
+    auto sim = Simulator({0x2485, 0x2905, 0xC503, 0, 0, 0,  0x2DB5});
+    sim.step();
+    sim.step();
+    sim.step();
+    sim.step();
+    REQUIRE(sim.getRegisters()[3] == 53);
+}
