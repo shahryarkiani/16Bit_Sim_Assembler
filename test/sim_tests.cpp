3d49e94f3ddbae3c733ed521d3a86147ab1cfbeb
Shahryar K <shahryarkiani04@gmail.com>
2023-05-19 17:43:26 -0400

    Write add opcode testcase

--- test/sim_tests.cpp
+++ test/sim_tests.cpp
@@ -12,3 +12,12 @@ TEST_CASE("TEST WRITE TO R0") {
     sim.step();
     REQUIRE(sim.getRegisters()[0] == (0));
 }
+
+TEST_CASE("TEST ADD TWO REGISTERS INTO THIRD") {
+    auto sim = Simulator({0x6401, 0x6801, 0x503});//Loads the same value into r1 and r2, then does r3 = r1+r2
+    sim.step();
+    sim.step();
+    sim.step();
+    REQUIRE(sim.getRegisters()[3] == (2 * (1<<6)));
+
+}
