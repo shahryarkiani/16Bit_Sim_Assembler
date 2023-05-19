2d3d55e29cd771ad312f7dababaa4fe865ef40c2
Shahryar K <shahryarkiani04@gmail.com>
2023-05-18 20:05:58 -0400

    Write failing test case for writing to r0

--- test/sim_tests.cpp
+++ test/sim_tests.cpp
@@ -6,3 +6,9 @@ TEST_CASE("TEST LOAD UPPER IMMEDIATE") {
     sim.step();
     REQUIRE(sim.getRegisters()[1] == (0x155 << 6));
 }
+
+TEST_CASE("TEST WRITE TO R0") {
+    auto sim = Simulator({0x63FF});//Loads a value into r0, which should always be 0
+    sim.step();
+    REQUIRE(sim.getRegisters()[0] == (0));
+}
