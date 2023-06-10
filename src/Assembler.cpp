0764090adf3edc97c63566d28f6c382c0b38cdab
Shahryar K <shahryarkiani04@gmail.com>
2023-06-10 17:54:59 -0400

    Define necessary methods and implement mapSymbols method

--- src/Assembler.cpp
+++ src/Assembler.cpp
@@ -4,6 +4,53 @@
 
 #include "Assembler.h"
 
-std::vector<uint16_t> Assembler::assemble(std::vector<std::string> tokens) {
+std::vector<uint16_t> Assembler::assemble(const std::vector<std::string>& tokens) {
+
+    //The first pass, this maps the symbols(like labels) to their actual position or value in the resulting machine code
+    auto symbolsMap = mapSymbols(tokens);
+
+    //The second pass then generates the machine code, while also replacing the symbols with their real values
+
+
     return std::vector<uint16_t>();
 }
+
+std::unordered_map<std::string, uint16_t> Assembler::mapSymbols(const std::vector<std::string> &tokens) {
+
+    std::unordered_map<std::string, uint16_t> symbolsMap;
+
+    int position = 0;
+
+    for(std::string token : tokens) {
+        if(isLabel(token)) {
+            symbolsMap.insert({ getLabel(token), position });
+        } else {
+            position += getOperationLength(getOperation(token));
+        }
+    }
+
+    return symbolsMap;
+}
+
+operation Assembler::getOperation(const std::string &token) {
+    //TODO
+    return operation::ADD;
+}
+
+bool Assembler::isLabel(const std::string &token) {
+    //TODO
+    return token == "";
+}
+
+std::string Assembler::getLabel(const std::string &token) {
+    //TODO
+    return std::string();
+}
+
+int Assembler::getOperationLength(operation op) {
+    return 0;
+}
+
+
+
+
--- src/Assembler.h
+++ src/Assembler.h
@@ -7,10 +7,22 @@
 #include <vector>
 #include <cstdint>
 #include <string>
+#include <unordered_map>
+
+enum class operation {
+    ADD, ADDIMMD, NAND, LOADUPPERIMMD, STOREW, LOADW, BRANCHEQUAl, JUMPANDLINK,//
+    NOOP, HALT, LOADLOWERIMMD, LOADIMMD, FILL, SPACE
+};
 
 class Assembler {
 public:
-    std::vector<uint16_t> assemble(std::vector<std::string> tokens);
+    std::vector<uint16_t> assemble(const std::vector<std::string>& tokens);
+private:
+    std::unordered_map<std::string, uint16_t> mapSymbols(const std::vector<std::string>& tokens);
+    operation getOperation(const std::string& token);
+    bool isLabel(const std::string& token);
+    std::string getLabel(const std::string& token);
+    int getOperationLength(operation op);
 };
 
 
