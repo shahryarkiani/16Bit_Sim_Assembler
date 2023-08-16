8639e4099d4a915b1fb81662a059a4a4345be8ad
Shahryar K <shahryarkiani04@gmail.com>
2023-08-16 16:34:31 -0400

    Implement passing assembler test

--- test/asm_tests.cpp
+++ test/asm_tests.cpp
@@ -1,6 +1,7 @@
 #include <catch2/catch_test_macros.hpp>
 #include <unordered_map>
 #include "../src/Assembler.h"
+#include "../src/Simulator.h"
 
 TEST_CASE("SYMBOL MAP TEST") {
 
@@ -13,4 +14,28 @@ TEST_CASE("SYMBOL MAP TEST") {
     auto actual = assembler.mapSymbols(tokens);
 
     REQUIRE(expected == actual);
+}
+
+TEST_CASE("BASIC ASSEMBLY TEST") {
+
+
+    auto assembler = Assembler();
+
+    std::string asm_code = "movi r1, 50 add r1, r1, r1 lli r2, 25  sw r1, r2, 0 halt";
+
+    auto tokens = tokenizeString(asm_code);
+
+    auto machineCode = assembler.assemble(tokens);
+
+    auto simulator = Simulator(machineCode);
+
+    while(!simulator.isHalted()) {
+        simulator.step();
+    }
+
+    REQUIRE(simulator.getMemory()[25] == 100);
+
+    REQUIRE(simulator.getRegisters()[1] == 100);
+    REQUIRE(simulator.getRegisters()[2] == 25);
+
 }
